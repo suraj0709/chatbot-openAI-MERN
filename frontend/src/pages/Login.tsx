30a4a7c674e46a6326745e2eb0ee091d2f5dfad8
Suraj <surajkr0709@gmail.com>
2023-12-16 11:06:39 +0530

    degined login page

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,7 +1,75 @@
 import React from "react";
+import { Box, Button, Typography } from "@mui/material";
+import CustomizedInput from "../components/shared/CustomizedInput";
+import { IoIosLogIn } from "react-icons/io";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <Box width={"100%"} height={"100%"} display={"flex"} flex={1}>
+      <Box padding={8} mt={8} display={{ md: "flex", sm: "none", xs: "none" }}>
+        <img src="airobot.png" alt="robot" style={{ width: "300px" }} />
+      </Box>
+      <Box
+        display={"flex"}
+        flex={{ xs: 1, md: 0.5 }}
+        justifyContent={"center"}
+        alignItems={"center"}
+        padding={2}
+        mt={16}
+        ml={"auto"}
+      >
+        <form
+          style={{
+            margin: "auto",
+            padding: "30px",
+            boxShadow: "10px 10px 20px #000",
+            borderRadius: "10px",
+            border: "none",
+          }}
+        >
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+            }}
+          >
+            <Typography
+              variant="h4"
+              textAlign="center"
+              padding={2}
+              fontWeight={600}
+            >
+              Login
+            </Typography>
+            <CustomizedInput name={"email"} type={"email"} label={"Email"} />
+            <CustomizedInput
+              name={"Password"}
+              type={"password"}
+              label={"Password"}
+            />
+            <Button
+              type="submit"
+              sx={{
+                px: 2,
+                py: 1,
+                mt: 2,
+                borderRadius: 2,
+                bgcolor: "#00fffc",
+                ":hover": {
+                  bgcolor: "white",
+                  color: "black",
+                },
+              }}
+              endIcon={<IoIosLogIn />}
+            >
+              Login
+            </Button>
+          </Box>
+        </form>
+      </Box>
+    </Box>
+  );
 };
 
 export default Login;
